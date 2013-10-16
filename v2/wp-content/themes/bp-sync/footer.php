7e1c4219c32fb8adacbccb03a1be48fc1e32c61c
James McKie <james.mckie@jmckie-l2.asynchrony.com>
2013-10-16 17:54:02 -0500

    adding content widgets

---
+++ v2/wp-content/themes/bp-sync/footer.php
@@ -0,0 +1,24 @@
+		</div> <!-- #container -->
+
+		<?php do_action( 'bp_after_container' ); ?>
+		<?php do_action( 'bp_before_footer'   ); ?>
+
+		<div id="footer">
+			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) || is_active_sidebar( 'second-footer-widget-area' ) || is_active_sidebar( 'third-footer-widget-area' ) || is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
+				<div id="footer-widgets">
+					<?php get_sidebar( 'footer' ); ?>
+				</div>
+			<?php endif; ?>
+
+
+			<?php do_action( 'bp_footer' ); ?>
+
+		</div><!-- #footer -->
+
+		<?php do_action( 'bp_after_footer' ); ?>
+
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html>
