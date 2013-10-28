7f0a298defad6cab55d935dad446425a21726f22
bj.self <bj.self@asolutions.com>
2013-10-28 11:50:42 -0500

    Added styles for header and basic body. -BJ

--- v2/wp-content/themes/bp-sync/header.php
+++ v2/wp-content/themes/bp-sync/header.php
@@ -1,37 +1,39 @@
 <!-- header.php -->
-<?php do_action( 'bp_before_header' ); ?>
 
-<div id="search-bar" role="search">
-  <div class="padder">
-    <h1 id="logo" role="banner"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>">
-      <?php bp_site_name(); ?>
-      </a></h1>
-
-    <!-- #search-form -->
-    
-    <?php do_action( 'bp_search_login_bar' ); ?>
-    <?php dynamic_sidebar( 'header-widgets' ); ?>
+<div class="inner">
+  <?php do_action( 'bp_before_header' ); ?>
+  <h1 id="logo" role="banner"><a href="<?php echo home_url(); ?>" title="<?php _ex( 'Home', 'Home page banner link title', 'buddypress' ); ?>">
+    <?php bp_site_name(); ?>
+    </a></h1>
+  <div id="navigation" role="navigation">
+    <?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'primary', 'fallback_cb' => 'bp_dtheme_main_nav' ) ); ?>
   </div>
-  <!-- .padder --> 
-</div>
-<!-- #search-bar -->
-<div class="avatar-container pull-right">
-  <div id="sidebar-me"> <a href="<?php echo bp_loggedin_user_domain(); ?>">
-    <?php bp_loggedin_user_avatar( 'type=thumb&width=40&height=40' ); ?>
-    </a>
-    <h4><?php echo bp_core_get_userlink( bp_loggedin_user_id() ); ?></h4>
-    <a class="button logout" href="<?php echo wp_logout_url( wp_guess_url() ); ?>">
-    <?php _e( 'Log Out', 'buddypress' ); ?>
-    </a>
-    <?php do_action( 'bp_sidebar_me' ); ?>
-  </div>
-</div>
-<!-- .avatar-container -->
+  
+  <div class="rightHeader">
+      <!-- #search-form -->
+      <div id="search-bar" role="search">
+        <?php do_action( 'bp_search_login_bar' ); ?>
+        <?php dynamic_sidebar( 'header-widgets' ); ?>
+      </div>
+      <!-- #search-bar -->
+      <div class="avatar-container">
+        <div id="sidebar-me">
+          <a href="<?php echo bp_loggedin_user_domain(); ?>">
+          	<?php bp_loggedin_user_avatar( 'type=thumb&width=40&height=40' ); ?>
+          </a>
+          <h4><?php echo bp_core_get_userlink( bp_loggedin_user_id() ); ?></h4>
+          <a class="button logout" href="<?php echo wp_logout_url( wp_guess_url() ); ?>">
+	          <?php _e( 'Log Out', 'buddypress' ); ?>
+          </a>
 
-<div id="navigation" role="navigation">
-  <?php wp_nav_menu( array( 'container' => false, 'menu_id' => 'nav', 'theme_location' => 'primary', 'fallback_cb' => 'bp_dtheme_main_nav' ) ); ?>
+          <?php do_action( 'bp_sidebar_me' ); ?>
+        </div>
+      </div>
+      <!-- .avatar-container -->
+      
+      <?php do_action( 'bp_header' ); ?>
+      <?php do_action( 'bp_after_header'     ); ?>
+      <?php do_action( 'bp_before_container' ); ?>
+    </div>
 </div>
-<?php do_action( 'bp_header' ); ?>
-<?php do_action( 'bp_after_header'     ); ?>
-<?php do_action( 'bp_before_container' ); ?>
 <!-- End header.php -->
