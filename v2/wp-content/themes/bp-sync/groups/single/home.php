3105e7df1419b3f743ad4766d320c3ab39204691
James McKie <james.mckie@jmckie-l2.asynchrony.com>
2013-11-01 16:54:06 -0500

    group page progress

--- v2/wp-content/themes/bp-sync/groups/single/home.php
+++ v2/wp-content/themes/bp-sync/groups/single/home.php
@@ -28,6 +28,57 @@
 			
 				</a>
 			</div><!-- #item-header-avatar -->
+			<div id="item-header-content">
+				<h3 class="<?php bp_group_type(); ?> icon">
+					<a href="http://codex.buddypress.org/user/setting-up-a-new-installation/group-settings-and-roles/">
+						<i class="icon-globe tips" title="this group is PUBLIC"></i> 
+						<i class="icon-lock tips" title="this group is PRIVATE"></i> 
+						<i class="icon-eye-close tips" title="this group is HIDDEN"></i> 
+					</a>
+					<?php bp_group_name(); ?></h3>
+				<?php do_action( 'bp_before_group_header_meta' ); ?>
+			
+				<div id="item-meta">
+			
+					<?php bp_group_description(); ?>
+			
+					<div id="item-buttons">
+			
+						<?php do_action( 'bp_group_header_actions' ); ?>
+			
+					</div><!-- #item-buttons -->
+			
+					<?php do_action( 'bp_group_header_meta' ); ?>
+			
+				</div>
+				<span class="activity"><?php printf( __( 'active %s', 'buddypress' ), bp_get_group_last_active() ); ?></span>
+			</div><!-- #item-header-content -->
+			<hr>
+			<div id="item-actions">
+			
+				<?php if ( bp_group_is_visible() ) : ?>
+			
+					<h4><?php _e( 'Group Admins', 'buddypress' ); ?></h4>
+			
+					<?php bp_group_list_admins();
+			
+					do_action( 'bp_after_group_menu_admins' );
+			
+					if ( bp_group_has_moderators() ) :
+						do_action( 'bp_before_group_menu_mods' ); ?>
+			
+						<h4><?php _e( 'Group Mods' , 'buddypress' ); ?></h4>
+			
+						<?php bp_group_list_mods();
+			
+						do_action( 'bp_after_group_menu_mods' );
+			
+					endif;
+			
+				endif; ?>
+			
+			</div><!-- #item-actions -->
+			
 		</div>
 		<div class="column size2of3 group-content">
 			<div id="item-nav">
