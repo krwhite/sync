e943dba4d62c5d1b1776116353732d1898c85a74
James McKie <james.mckie@jmckie-l2.asynchrony.com>
2013-11-06 10:07:20 -0600

    archive styling

--- v2/wp-content/themes/bp-sync/archive.php
+++ v2/wp-content/themes/bp-sync/archive.php
@@ -12,59 +12,57 @@
 	  <?php get_sidebar('sidebar-1'); ?>
   <?php endif; ?>
   <div id="pageContent">
-			<?php if ( is_active_sidebar( 'content_widgets_top' ) ) : ?>
-				<ul id="contentWidgets">
-					<?php dynamic_sidebar( 'content_widgets_top' ); ?>
-				</ul>
-			<?php endif; ?>
+		<?php if ( is_active_sidebar( 'content_widgets_top' ) ) : ?>
+			<ul id="contentWidgets">
+				<?php dynamic_sidebar( 'content_widgets_top' ); ?>
+			</ul>
+		<?php endif; ?>
 
 		<?php do_action( 'bp_before_archive' ); ?>
 
 		<div class="page" id="blog-archives" role="main">
 
 			<h1><?php printf( __( '%1$s', 'buddypress' ), wp_title( false, false ) ); ?></h1>
-
-			<?php if ( have_posts() ) : ?>
-
-				<?php bp_dtheme_content_nav( 'nav-above' ); ?>
-
-				<?php while (have_posts()) : the_post(); ?>
-
-					<?php do_action( 'bp_before_blog_post' ); ?>
-
-					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-
-						<div class="post-content">
-							<strong class="posttitle"><a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></strong>
-
-							<div class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?></div>
-
-							<div class="tag-list">
-								<i class="icon-tags"></i> <?php the_tags( '<span class="tags">' . __( '', 'buddypress' ), ', ', '</span>' ); ?> 
+			<p><?php echo category_description( $category_id ); ?></p>
+			<div class="post-list">
+				<?php if ( have_posts() ) : ?>
+	
+					<?php while (have_posts()) : the_post(); ?>
+	
+						<?php do_action( 'bp_before_blog_post' ); ?>
+	
+						<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	
+							<div class="post-content row">
+								<div class="column size3of5">
+									<a class="article-link" href="<?php the_permalink(); ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'buddypress' ); ?> <?php the_title_attribute(); ?>"><strong class="posttitle"><?php the_title(); ?></strong>
+										<span class="date"><?php printf( __( '%1$s <span>in %2$s</span>', 'buddypress' ), get_the_date(), get_the_category_list( ', ' ) ); ?></span>
+									</a>
+								</div>
+								<div class="tag-list column size2of5">
+									<i class="icon-tags"></i> <?php the_tags( '<span class="tags">' . __( '', 'buddypress' ), ' ', '</span>' ); ?> 
+								</div>
 							</div>
+	
 						</div>
-
+	
+						<?php do_action( 'bp_after_blog_post' ); ?>
+	
+					<?php endwhile; ?>
+	
+					<?php bp_dtheme_content_nav( 'nav-below' ); ?>
+	
+				<?php else : ?>
+					<div class="item-body">
+						<p><?php _e( 'Hmmm, looks like we don&rsquo;t have anything for this category yet.', 'buddypress' ); ?></p>
 					</div>
-
-					<?php do_action( 'bp_after_blog_post' ); ?>
-
-				<?php endwhile; ?>
-
-				<?php bp_dtheme_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-				<div class="item-body">
-					<p><?php _e( 'Hmmm, looks like we don&rsquo;t have anything for this category yet.', 'buddypress' ); ?></p>
-				</div>
-			<?php endif; ?>
-
-		</div>
+				<?php endif; ?>
+			</div><!-- .post-list -->
+		</div><!-- .page -->
 
 		<?php do_action( 'bp_after_archive' ); ?>
 
-		</div><!-- .padder -->
-	</div><!-- #content -->
   </div><!-- End #pageContent -->
 </div><!-- End #main -->
 <div id="footer">
