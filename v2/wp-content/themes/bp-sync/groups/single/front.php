c1d494c2286d751ac8bd502da8a6a04415bc5b91
James McKie <james.mckie@jmckie-l2.asynchrony.com>
2013-11-07 11:27:21 -0600

    removed unused templates

--- v2/wp-content/themes/bp-sync/groups/single/front.php
+++
@@ -1,7 +0,0 @@
-<?php do_action( 'bp_before_group_activity_content' ); ?>
-
-<div class="activity single-group" role="main">
-	<?php locate_template( array( 'forum.php' ), true ); ?>
-</div><!-- .activity.single-group -->
-
-<?php do_action( 'bp_after_group_activity_content' ); ?>
