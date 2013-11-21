7200ac7612d397606cf6edf9c09618efa2cd97da
James McKie <james.mckie@jmckie-l2.asynchrony.com>
2013-11-21 16:06:06 -0600

    fixes for IE9, added missing font files

--- v2/wp-content/themes/bp-sync/html-head.php
+++ v2/wp-content/themes/bp-sync/html-head.php
@@ -9,7 +9,7 @@
     	<?php wp_title( '|', true, 'right' ); bloginfo( 'name' ); ?>
     </title>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<!--[if IE 8]> <link rel="stylesheet" type="text/css" href="/wp-content/themes/bp-sync/style-ie.css" /> <![endif]-->
+	<!--[if lt IE 10]> <link rel="stylesheet" type="text/css" href="/wp-content/themes/bp-sync/style-ie.css" /> <![endif]-->
     <?php bp_head(); ?>
     <?php wp_head(); ?>
 </head>
